c5d81e8d89839f55ab8a9e1f0e3785237f311b5f
Prime-Cave <tomisin.akinfemiwa@gmail.com>
2025-04-01 08:44:17 +0200

    new changes

---
+++ main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+const (
+	PhotoApi = "https://api.pexels.com/v1"
+	VideoApi = "https://api.pexels.com/videos"
+)
+
+type Client struct {
+	Token          string
+	hc             http.Client
+	RemainingTimes int32
+}
+
+func NewClient(token string) *Client {
+	c := http.Client{}
+	return &Client{
+		Token: token,
+		hc:    c,
+	}
+}
+
+type SearchResult struct {
+	Page         int32   `json:"page"`
+	PerPage      int32   `json:"per_page"`
+	TotalResults int32   `json:"total_results"`
+	NextPage     string  `json:"next_page"`
+	Photos       []Photo `json:"photos"`
+}
+
+type Photo struct {
+	Id              int32       `json:"id"`
+	Width           int32       `json:"width"`
+	Height          int32       `json:"height"`
+	Url             string      `json:"url"`
+	Photographer    string      `json:"photographer"`
+	PhotographerUrl string      `json:"photographer_url"`
+	Src             PhotoSource `json:"src"`
+}
+
+type PhotoSource struct {
+	Original  string `json:"original"`
+	Large     string `json:"large"`
+	Large2x   string `json:"large_2x"`
+	Medium    string `json:"medium"`
+	Small     string `json:"small"`
+	Portrait  string `json:"portrait"`
+	Square    string `json:"square"`
+	Landscape string `json:"landscape"`
+	Tiny      string `json:"tiny"`
+}
+
+func (c *Client) SearchPhotos(query string, perPage, page int) (*SearchResult, error) {
+	url := fmt.Sprintf(PhotoApi+"/search?query=%s&per_page=%d&page=%d", query, perPage, page)
+	res, err := c.requestDoWithAuth("GET", url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	var result SearchResult
+	err = json.Unmarshal(data, &result)
+
+	return &result, err
+}
+
+func (c *Client) CuratedPhotos(perPage, page int)(*CuratedResult, error){
+	url := fmt.Sprintf(PhotoApi+"/curated?per_page=%d&page=%d", perPage, page)
+	res, err := c.requestDoWithAuth("GET", url)
+	if err != nil {
+		return nil, err 
+	}
+
+	defer res.Body.Close()
+
+	data, err:= io.ReadAll(res.Body)
+	if err != nil{
+		return nil, err
+	}
+	var result CuratedResult
+	err = json.Unmarshal(data, &result)
+	return &result, err
+}
+
+func (c *Client) requestDoWithAuth(method, url string) (*http.Response, error){
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", c.Token)
+	res, err := c.hc.Do(req)
+	if err != nil {
+		return res, err
+	}
+	times, err := strconv.Atoi(res.Header.Get("X-Ratelimit-Remaining"))
+	if err != nil{
+		return res, nil
+	} else {
+		c.RemainingTimes = int32(times)
+	}
+	return res, nil
+}
+
+func main() {
+	var TOKEN = os.Getenv("PEXELSTOKEN")
+
+	var c = NewClient(TOKEN)
+	result, err := c.SearchPhotos("waves", 15, 1)
+	if err != nil {
+		log.Fatalf("Search Error: %v", err)
+	}
+	if result.Page == 0 {
+		fmt.Errorf("Search result is wrong")
+	}
+	fmt.Println(result)
+}
