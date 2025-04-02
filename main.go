2274ea9046c8d225a08075214879c420f7616ad1
Prime-Cave <tomisin.akinfemiwa@gmail.com>
2025-04-02 11:36:41 +0200

    Updating the code

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"strconv"
 	"time"
+	"github.com/joho/godotenv"
 )
 
 const (
@@ -69,23 +70,46 @@ type PhotoSource struct {
 }
 
 type VideoSearchResult struct {
-	 
+	Page         int32   `json:"page"`
+	PerPage      int32   `json:"per_page"`
+	TotalResults int32   `json:"total_results"`
+	NextPage     string  `json:"next_page"`
+	Videos       []Video `json:"videos"`
 }
 
-type Video struct{
-
+type Video struct {
+	Id            int32           `json:"id"`
+	Width         int32           `json:"width"`
+	Height        int32           `json:"height"`
+	Url           string          `json:"url"`
+	Image         string          `json:"image"`
+	FullRes       interface{}     `json:"full_res"`
+	Duration      float64         `json:"duration"`
+	VideoFiles    []VideoFiles    `json:"video_files"`
+	VideoPictures []VideoPictures `json:"video_pictures"`
 }
 
 type PopularVideos struct {
-
+	Page         int32   `json:"page"`
+	PerPage      int32   `json:"per_page"`
+	TotalResults int32   `json:"total_results"`
+	Url          string  `json:"url"`
+	Videos       []Video `json:"videos"`
 }
 
-type VideoFiles struct{
-
+type VideoFiles struct {
+	Id       int32  `json:"id"`
+	Quality  string `json:"quality"`
+	FileType string `json:"file_type"`
+	Width    int32  `json:"width"`
+	Height   int32  `json:"height"`
+	Link     string `json:"link"`
 }
 
 type VideoPictures struct {
-
+	Id      int32  `json:"id"`
+	Picture string `json:"picture"`
+	Nr      int32  `json:"nr"`
 }
 
 func (c *Client) SearchPhotos(query string, perPage, page int) (*SearchResult, error) {
@@ -169,28 +193,71 @@ func (c *Client) GetRandomPhoto() (*Photo, error) {
 	return nil, err
 }
 
-func (c *Client) SearchVideo(query, perPage, page int) (*VideoSearchResult, error) {
+func (c *Client) SearchVideo(query string, perPage, page int) (*VideoSearchResult, error) {
+	url := fmt.Sprintf(VideoApi+"/search?query=%s&per_page=%d&page=%d", query,perPage, page)
+	res, err := c.requestDoWithAuth("GET", url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
 
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var result VideoSearchResult
+	json.Unmarshal(data, &result)
+	return &result, err
 }
 
 func (c *Client) PopularVideo(perPage, page int) (*PopularVideos, error) {
+	url := fmt.Sprintf(VideoApi + "/popular?per_page=%d&page=%d", perPage, page)
+	res, err := c.requestDoWithAuth("GET", url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
 
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	var result PopularVideos
+	err = json.Unmarshal(data, &result)
+	return &result, err
 }
 
 func (c *Client) GetRandomVideo() (*Video, error) {
+	rand.NewSource(time.Now().Unix())
+	randNum := rand.Intn(1001)
+	result, err := c.PopularVideo(1, randNum)
+	if err==nil && len(result.Videos) ==1 {
+		return &result.Videos[0], nil
+	}
+	return nil , err
+}
 
+func (c *Client) GetRemainingRequestsInThisMonth() int32{
+	return c.RemainingTimes
 }
 
 func main() {
+	err := godotenv.Load() // Load .env file
+	if err != nil {
+			log.Fatalf("Error loading .env file: %v", err)
+	}
+
 	var TOKEN = os.Getenv("PEXELSTOKEN")
+	fmt.Println("Token:", TOKEN) // Print the token
 
 	var c = NewClient(TOKEN)
-	result, err := c.SearchPhotos("waves", 15, 1)
+	result, err := c.SearchVideo("waves", 15, 1)
 	if err != nil {
-		log.Fatalf("Search Error: %v", err)
+			log.Fatalf("Search Error: %v", err)
 	}
 	if result.Page == 0 {
-		fmt.Errorf("Search result is wrong")
+			fmt.Errorf("Search result is wrong")
 	}
 	fmt.Println(result)
 }
